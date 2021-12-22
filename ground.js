3eeddfea2bfa63165a592f5e21d4157617c91d98
Thiccccchungus <89295986+Thiccccchungus@users.noreply.github.com>
2021-12-22 16:21:37 +0800

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,27 @@
+class Ground{
+    constructor(x,y,w,h){
+        
+        var options={
+            isStatic:true
+        }
+        this.x=x
+        this.y=y
+        this.w=w
+        this.h=h
+        this.body=Bodies.rectangle(x,y,w,h,options)
+        World.add(world,this.body)
+        
+    }
+    show(){
+        var groundpos=this.body.position
+        push();
+        translate(groundpos.x,groundpos.y)
+        rectMode(CENTER)
+        strokeWeight(5)
+        stroke("WHITE")
+        fill("Black")
+        rect(0,0,this.w,this.h)
+        pop();
+    }
+}
+
